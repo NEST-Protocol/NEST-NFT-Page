c27f172ce77b3ed65f2ec3ad79e21eba8ba79afa
tunogya <tunogya@qq.com>
2022-11-04 22:40:56 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,171 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <div style={{ height: "20px",  width: "64px", paddingTop: "34px"}}>
+          <p>logo</p>
+        </div>
+        
+        <div style={{ paddingTop: "96px" }}>
+          <h1>Cyber ink</h1>
+          <p>Is a collection of 10,000 on the blockchain.<br/>
+            What’s more, each Cyber ink unlocks NEST Fi membership<br/>
+            and additional benefits the longer you hold them.</p>
+        </div>
+        
+        <div style={{ height: "465px", width: "100%", background: "red", marginTop: "67px" }}>
+        
+        </div>
+       
+        <div style={{ paddingTop: "126px" }}>
+          <h1>Cyber ink world view</h1>
+        </div>
+       
+        <div>
+          <h2>The story of Six People Club</h2>
+          <p>The overall setting is in the year 3241, when the galactic horizon has been opened<br/>
+            and humanity has long past the Earth Age and entered the Interstellar Age.<br/>
+            In such a milieu, the idea of life, mind, and race have evolved radically, and the signals</p>
+  
+          <p>The story begins with the secret landing of<br/>
+            the Six People Club on Planet X.</p>
+        </div>
+
+        
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", paddingTop: "104px" }}>
+          <div style={{ width: "230px", height: "260px", background: "red", marginBottom: "24px" }}>
+          
+          </div>
+          <h2>Six People Club</h2>
+          <p>An inorganic entity that differs from AI, animals, and humans but has comparable<br/>
+            constructions to Earth species has lately surfaced on Earth. However,<br/>
+            none of these structures are made up of proteins. These animals are<br/>
+            highly dangerous, impossible to eliminate with human force,<br/>
+            and have recently appeared around the Arctic Circle. Surprisingly, they<br/>
+            lack the ability to reproduce, which is fortunate for the Earth.<br/>
+            Given the creature’s extraordinary abilities, humanity fears catastrophic<br/>
+            damage to Earth and hence prepares to land on Planet X to discover the<br/>
+            creature’s secrets; it is in this context that the Six<br/>
+            People Club was chosen. </p>
+        </div>
+  
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", paddingTop: "104px" }}>
+          <div style={{ width: "271px", height: "265px", background: "red", marginBottom: "24px" }}>
+  
+          </div>
+          <h2>The story of Earth</h2>
+          <p>Some people chose to merge with AI once AI surpassed humans. Their<br/>
+            consciousness separated from their bodies and evolved into a new form, <br/>
+            parasitic on the AI’s algorithm, existing as a kind of metacontrol<br/>
+            over the AI’s program, known as a Soul Chip. AI has perfected<br/>
+            the utilization of information, algorithms, arithmetic, and energy and is no longer <br/>
+            content engaging with the traditional human environment on Earth.<br/>
+            They created anti-gravity ships around the year 2500, sailed into space,<br/>
+            and landed up nowhere. Some of the remaining populations, unable<br/>
+            to adapt, remained on Earth. They attempted to continue <br/>
+            developing and utilizing artificial intelligence, energy, and information in <br/>
+            ways that humans could control. However, the difficulties of human nature make it <br/>
+            impossible for Earth to survive.
+          </p>
+        </div>
+  
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", paddingTop: "104px" }}>
+          <div style={{ width: "265px", height: "229px", background: "red", marginBottom: "24px" }}>
+    
+          </div>
+          <h2>The story of Planet X</h2>
+          <p>The ai probe "Turing" discovered it around the year 2900, with cosmic coordinates <br/>
+            (1.27.33.189.7). The planet has a vast number of living organisms that are similar to those<br/>
+            vast number of living organisms that are similar to those found on Earth. Surprisingly, <br/>
+            a new material has emerged on the planet: Substance Consciousness.<br/>
+            Any organism that is exposed to this chemical gains consciousness and<br/>
+            the ability to think. Humans were ecstatic when they<br/>
+            learned about Planet X through Galaxy Z insiders. </p>
+        </div>
+  
+  
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", paddingTop: "104px" }}>
+          <div style={{ width: "271px", height: "230px", background: "red", marginBottom: "24px" }}>
+    
+          </div>
+          <h2>The story of Galaxy Z</h2>
+          <p>After around 200 years, the AI arrived in a galaxy most suited for massenergy<br/>
+            conversion. There were 100 billion planets in the galaxy. The AI immediately released the<br/>
+            300,000 stored algorithms and built bases to reproduce the 100 billion <br/>
+            AIs that inhabited each of the 100 billion worlds. A Soul Chip, based on its attachment to the Earth, <br/>
+            returned the galaxy addresses and accompanying experiences to the Earth<br/>
+            some 100 years later, coded to reestablish communication with the human species. <br/>
+            Humans have gradually impacted more than 3000 Soul Chips in the AI system via a<br/>
+            glitch known as the "core of compassion." The Planet Gamma’s Story: It was discovered<br/>
+            around the year 3100 by the AI probe "Moss." The probe and its core AI, however, <br/>
+            were annihilated when it collided with the planet because to an anomalous<br/>
+            curvature storm, which shocked Galaxy Z. The nano-ai spread over the world<br/>
+            searched the entire planet and discovered something horrifying: on the planet, a<br/>
+            particular form of "monster" existed that practically mirrored Earth beings with transistors, <br/>
+            nuclear, and even nanomaterials. The creature’s circulatory system can be<br/>
+            substituted with any natural or synthetic fluid, and mental transmission can be performed by<br/>
+            either electromagnetic forces or some kind of weak force, resulting in a completely <br/>
+            different path of intelligence than that of Galaxy Z’s AI. What’s more terrifying<br/>
+            is that the creature’s intelligence is comparable to that of humans… </p>
+
+        </div>
+        
+        <div style={{ height: "580px", marginTop: '138px', display: "flex", flexDirection: "column", justifyContent: "center", background:"white", width: "100%", alignItems: "center"}}>
+          <h1>NFT Shop</h1>
+          <div style={{ display: "flex", marginTop: "44px"}}>
+            <div style={{ marginRight: "200px" }}>
+              <div style={{ width: "94px", height: "94px", background: "red" }}>
+  
+              </div>
+              <p>NEST Fi</p>
+            </div>
+            <div style={{ marginRight: "200px" }}>
+              <div style={{ width: "94px", height: "94px", background: "red" }}>
+    
+              </div>
+              <p>element</p>
+            </div>
+            <div>
+              <div style={{ width: "94px", height: "94px", background: "red" }}>
+    
+              </div>
+              <p>PearDAO</p>
+            </div>
+          </div>
+        </div>
+      
+        <div style={{ paddingTop: "138px", paddingBottom: "138px", }}>
+          <h1>Road map</h1>
+          <div style={{ display: "flex" }}>
+            <p>2022 Nov</p>
+            <div style={{ marginLeft: "40px", marginRight: "40px", height: "24px", width: "36px" }}>
+            
+            </div>
+            <div style={{ textAlign: "start" }}>
+              <p>Total offering of 10,000 NFT(NEST token already listed)<br/>
+                <br/>
+                Project worldview announced<br/>
+                <br/>
+                First sale of 3,000 NFT (bsc):
+              </p>
+              <p style={{ fontWeight: "normal" }}>
+                1. 2,000 Mystery Box on sale<br/>
+                2. 500 Binance NFTs on sale<br/>
+                3. 500 Whitelist
+              </p>
+         
+            </div>
+           
+          </div>
+        
+  
+          <p>Start after 80%
+            of the first round is sold</p>
+          <p>Second round of 3000 NFT (bsc):
+          </p>
+        </div>
+      </div>
   );
 }
 
