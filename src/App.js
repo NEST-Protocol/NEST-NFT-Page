89f026c42642798403bceba74c84389cbb0cc183
tunogya <tunogya@qq.com>
2022-11-05 15:58:16 +0800

    update

--- src/App.js
+++ src/App.js
@@ -12,38 +12,49 @@ import point from "./assets/point@2x.png"
 
 function App() {
   return (
-      <div style={{textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center", backgroundImage: "url(bg.jpg)", backgroundPosition: "top"}}>
-        <img src={logo} alt={"NEST"} style={{height: "20px", width: "64px", paddingTop: "34px"}}/>
+      <div style={{
+        textAlign: "center",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        backgroundImage: "url(bg.jpg)",
+        backgroundPosition: "top",
+        backgroundSize: "cover",
+      }}>
+        <a href={"https://nestprotocol.org/"} target={"_blank"} rel={"noreferrer"}>
+          <img src={logo} alt={"NEST"} style={{height: "20px", width: "64px", paddingTop: "34px"}}/>
+        </a>
         
         <div style={{paddingTop: "96px"}}>
           <h1>Cyber ink</h1>
-          <p style={{ marginTop: '15px' }}>Is a collection of 10,000 on the blockchain.<br/>
+          <p style={{marginTop: '15px'}}>Is a collection of 10,000 on the blockchain.<br/>
             What’s more, each Cyber ink unlocks NEST Fi membership<br/>
             and additional benefits the longer you hold them.</p>
         </div>
         
         <img src={_4nft} alt={'4nft'} style={{height: "465px", marginTop: "67px"}}/>
         
-        <div style={{     width: "100%", background: "rgba(255, 255, 255, 0.6)"}}>
+        <div style={{width: "100%", background: "rgba(255, 255, 255, 0.6)"}}>
           <div style={{paddingTop: "126px"}}>
             <h1>Cyber ink world view</h1>
           </div>
-  
-          <div style={{ marginTop: "44px" }}>
+          
+          <div style={{marginTop: "44px"}}>
             <h2>The story of Six People Club</h2>
-            <p style={{ marginTop: "22px" }}>The overall setting is in the year 3241, when the galactic horizon has been opened<br/>
+            <p style={{marginTop: "22px"}}>The overall setting is in the year 3241, when the galactic horizon has been
+              opened<br/>
               and humanity has long past the Earth Age and entered the Interstellar Age.<br/>
               In such a milieu, the idea of life, mind, and race have evolved radically, and the signals</p>
             <br/>
             <p>The story begins with the secret landing of<br/>
               the Six People Club on Planet X.</p>
           </div>
-  
+          
           <div style={{display: "flex", flexDirection: "column", alignItems: "center", paddingTop: "104px"}}>
             <div style={{width: "230px", height: "260px", marginBottom: "24px"}}>
               <img src={nft1} alt={'nft1'} style={{width: "100%", height: "100%"}}/>
             </div>
-            <h2 style={{ marginTop: "44px", marginBottom: "24px" }}>Six People Club</h2>
+            <h2 style={{marginTop: "44px", marginBottom: "24px"}}>Six People Club</h2>
             <p>An inorganic entity that differs from AI, animals, and humans but has comparable<br/>
               constructions to Earth species has lately surfaced on Earth. However,<br/>
               none of these structures are made up of proteins. These animals are<br/>
@@ -55,12 +66,12 @@ function App() {
               creature’s secrets; it is in this context that the Six<br/>
               People Club was chosen. </p>
           </div>
-  
+          
           <div style={{display: "flex", flexDirection: "column", alignItems: "center", paddingTop: "104px"}}>
             <div style={{width: "271px", height: "265px", marginBottom: "24px"}}>
               <img src={nft2} alt={'nft2'} style={{width: "100%", height: "100%"}}/>
             </div>
-            <h2 style={{ marginTop: "44px", marginBottom: "24px" }}>The story of Earth</h2>
+            <h2 style={{marginTop: "44px", marginBottom: "24px"}}>The story of Earth</h2>
             <p>Some people chose to merge with AI once AI surpassed humans. Their<br/>
               consciousness separated from their bodies and evolved into a new form, <br/>
               parasitic on the AI’s algorithm, existing as a kind of metacontrol<br/>
@@ -75,12 +86,12 @@ function App() {
               impossible for Earth to survive.
             </p>
           </div>
-  
+          
           <div style={{display: "flex", flexDirection: "column", alignItems: "center", paddingTop: "104px"}}>
             <div style={{width: "265px", height: "229px", marginBottom: "24px"}}>
               <img src={nft3} alt={'nft3'} style={{width: "100%", height: "100%"}}/>
             </div>
-            <h2 style={{ marginTop: "44px", marginBottom: "24px" }}>The story of Planet X</h2>
+            <h2 style={{marginTop: "44px", marginBottom: "24px"}}>The story of Planet X</h2>
             <p>The ai probe "Turing" discovered it around the year 2900, with cosmic coordinates <br/>
               (1.27.33.189.7). The planet has a vast number of living organisms that are similar to those<br/>
               vast number of living organisms that are similar to those found on Earth. Surprisingly, <br/>
@@ -89,16 +100,23 @@ function App() {
               the ability to think. Humans were ecstatic when they<br/>
               learned about Planet X through Galaxy Z insiders. </p>
           </div>
-  
-          <div style={{display: "flex", flexDirection: "column", alignItems: "center", paddingTop: "104px", paddingBottom: "120px"}}>
+          
+          <div style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            paddingTop: "104px",
+            paddingBottom: "120px"
+          }}>
             <div style={{width: "271px", height: "230px", marginBottom: "24px"}}>
               <img src={nft4} alt={'nft4'} style={{width: "100%", height: "100%"}}/>
             </div>
-            <h2 style={{ marginTop: "44px", marginBottom: "24px" }}>The story of Galaxy Z</h2>
+            <h2 style={{marginTop: "44px", marginBottom: "24px"}}>The story of Galaxy Z</h2>
             <p>After around 200 years, the AI arrived in a galaxy most suited for massenergy<br/>
               conversion. There were 100 billion planets in the galaxy. The AI immediately released the<br/>
               300,000 stored algorithms and built bases to reproduce the 100 billion <br/>
-              AIs that inhabited each of the 100 billion worlds. A Soul Chip, based on its attachment to the Earth, <br/>
+              AIs that inhabited each of the 100 billion worlds. A Soul Chip, based on its attachment to the
+              Earth, <br/>
               returned the galaxy addresses and accompanying experiences to the Earth<br/>
               some 100 years later, coded to reestablish communication with the human species. <br/>
               Humans have gradually impacted more than 3000 Soul Chips in the AI system via a<br/>
@@ -129,21 +147,21 @@ function App() {
           <h1>NFT Shop</h1>
           <div style={{display: "flex", marginTop: "78px"}}>
             <div style={{marginRight: "200px"}}>
-              <div style={{width: "94px", height: "94px", marginBottom: "24px"}}>
-                <img src={nestfi} alt={'nestfi'} style={{width: "100%", height: "100%"}}/>
-              </div>
+              <a href={"https://finance.nestprotocol.org/"} target={"_blank"}>
+                <img src={nestfi} alt={'nestfi'} style={{width: "94px", height: "94px", marginBottom: "24px"}}/>
+              </a>
               <p>NEST Fi</p>
             </div>
             <div style={{marginRight: "200px"}}>
-              <div style={{width: "94px", height: "94px",  marginBottom: "24px"}}>
-                <img src={element} alt={'element'} style={{width: "100%", height: "100%"}}/>
-              </div>
+              <a href={"https://www.element.market/"} target={"_blank"}>
+                <img src={element} alt={'element'} style={{width: "94px", height: "94px", marginBottom: "24px"}}/>
+              </a>
               <p>element</p>
             </div>
             <div>
-              <div style={{width: "94px", height: "94px",  marginBottom: "24px"}}>
-                <img src={peardao} alt={'peardao'} style={{width: "100%", height: "100%"}}/>
-              </div>
+              <a href={"https://peardao.io/"} target={"_blank"}>
+                <img src={peardao} alt={'peardao'} style={{width: "94px", height: "94px", marginBottom: "24px"}}/>
+              </a>
               <p>PearDAO</p>
             </div>
           </div>
@@ -151,12 +169,19 @@ function App() {
         
         <div style={{paddingTop: "138px", paddingBottom: "138px",}}>
           <h1>Road map</h1>
-          <div style={{ width: "1px", height: "1380px", background: "#C9C9C9", position: "absolute", left: "50%", marginTop: "40px" }}/>
+          <div style={{
+            width: "1px",
+            height: "1380px",
+            background: "#C9C9C9",
+            position: "absolute",
+            left: "50%",
+            marginTop: "40px"
+          }}/>
           <div>
             <div style={{display: "flex", marginTop: '68px'}}>
               <p style={{fontSize: "25px", fontWeight: "bold", width: "450px", textAlign: "right"}}>2022 Nov</p>
               <div style={{marginLeft: "40px", marginRight: "40px", height: "24px", width: "36px"}}>
-                <img src={point} alt={'point'} style={{ width: "100%", height: "100%" }}/>
+                <img src={point} alt={'point'} style={{width: "100%", height: "100%"}}/>
               </div>
               <div style={{textAlign: "start", width: "450px"}}>
                 <p>Total offering of 10,000 NFT(NEST token already listed)<br/>
@@ -172,12 +197,12 @@ function App() {
                 </p>
               </div>
             </div>
-  
+            
             <div style={{display: "flex", marginTop: '70px'}}>
               <p style={{fontSize: "25px", fontWeight: "bold", width: "450px", textAlign: "right"}}>Start after 80%<br/>
                 of the first round is sold</p>
               <div style={{marginLeft: "40px", marginRight: "40px", height: "24px", width: "36px"}}>
-                <img src={point} alt={'point'} style={{ width: "100%", height: "100%" }}/>
+                <img src={point} alt={'point'} style={{width: "100%", height: "100%"}}/>
               </div>
               <div style={{textAlign: "start", width: "450px"}}>
                 <p>Second round of 3000 NFT (bsc): </p>
@@ -196,12 +221,13 @@ function App() {
                 </p>
               </div>
             </div>
-  
+            
             <div style={{display: "flex", marginTop: '70px'}}>
-              <p style={{fontSize: "25px", fontWeight: "bold", width: "450px", textAlign: "right"}}>Start after 100%<br/>
+              <p style={{fontSize: "25px", fontWeight: "bold", width: "450px", textAlign: "right"}}>Start after
+                100%<br/>
                 of the first round is sold</p>
               <div style={{marginLeft: "40px", marginRight: "40px", height: "24px", width: "36px"}}>
-                <img src={point} alt={'point'} style={{ width: "100%", height: "100%" }}/>
+                <img src={point} alt={'point'} style={{width: "100%", height: "100%"}}/>
               </div>
               <div style={{textAlign: "start", width: "450px"}}>
                 <p>NFT Utility</p>
@@ -212,12 +238,13 @@ function App() {
                 </p>
               </div>
             </div>
-  
+            
             <div style={{display: "flex", marginTop: '70px'}}>
-              <p style={{fontSize: "25px", fontWeight: "bold", width: "450px", textAlign: "right"}}>Fist and second start<br/>
+              <p style={{fontSize: "25px", fontWeight: "bold", width: "450px", textAlign: "right"}}>Fist and second
+                start<br/>
                 after 80% of rounds sold</p>
               <div style={{marginLeft: "40px", marginRight: "40px", height: "24px", width: "36px"}}>
-                <img src={point} alt={'point'} style={{ width: "100%", height: "100%" }}/>
+                <img src={point} alt={'point'} style={{width: "100%", height: "100%"}}/>
               </div>
               <div style={{textAlign: "start", width: "450px"}}>
                 <p>Brand collaboration exclusive NFT on sale</p>
@@ -233,12 +260,13 @@ function App() {
                 </p>
               </div>
             </div>
-  
+            
             <div style={{display: "flex", marginTop: '70px'}}>
-              <p style={{fontSize: "25px", fontWeight: "bold", width: "450px", textAlign: "right"}}>Start after 80% of<br/>
+              <p style={{fontSize: "25px", fontWeight: "bold", width: "450px", textAlign: "right"}}>Start after 80%
+                of<br/>
                 the second round is sold</p>
               <div style={{marginLeft: "40px", marginRight: "40px", height: "24px", width: "36px"}}>
-                <img src={point} alt={'point'} style={{ width: "100%", height: "100%" }}/>
+                <img src={point} alt={'point'} style={{width: "100%", height: "100%"}}/>
               </div>
               <div style={{textAlign: "start", width: "450px"}}>
                 <p>Third round of 3000 NFT (eth):</p>
@@ -249,12 +277,12 @@ function App() {
                 </p>
               </div>
             </div>
-  
+            
             <div style={{display: "flex", marginTop: '70px'}}>
               <p style={{fontSize: "25px", fontWeight: "bold", width: "450px", textAlign: "right"}}>80% of all sold<br/>
                 the second round is sold</p>
               <div style={{marginLeft: "40px", marginRight: "40px", height: "24px", width: "36px"}}>
-                <img src={point} alt={'point'} style={{ width: "100%", height: "100%" }}/>
+                <img src={point} alt={'point'} style={{width: "100%", height: "100%"}}/>
               </div>
               <div style={{textAlign: "start", width: "450px"}}>
                 <p>Cross Platform</p>
@@ -263,12 +291,13 @@ function App() {
                 </p>
               </div>
             </div>
-  
+            
             <div style={{display: "flex", marginTop: '70px'}}>
-              <p style={{fontSize: "25px", fontWeight: "bold", width: "450px", textAlign: "right"}}>100% of all sold<br/>
+              <p style={{fontSize: "25px", fontWeight: "bold", width: "450px", textAlign: "right"}}>100% of all
+                sold<br/>
                 the second round is sold</p>
               <div style={{marginLeft: "40px", marginRight: "40px", height: "24px", width: "36px"}}>
-                <img src={point} alt={'point'} style={{ width: "100%", height: "100%" }}/>
+                <img src={point} alt={'point'} style={{width: "100%", height: "100%"}}/>
               </div>
               <div style={{textAlign: "start", width: "450px"}}>
                 <p>P2E (gamefi)</p>
